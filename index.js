b0d468791fcf9a2686e90ec5177973bdee3b4440
JinYeongKim <handayoung11@gmail.com>
2022-04-28 22:29:36 +0900

    공격 기능을 위한 로직 작성

--- index.js
+++ index.js
@@ -9,21 +9,46 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7;
 
 class Sprite {
-  constructor({ position, velocity }) {
+  constructor({ position, velocity, color = "red", offset }) {
     this.position = position;
     this.velocity = velocity;
     this.height = 150;
+    this.width = 50;
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset,
+      width: 100,
+      height: 50,
+    };
+    this.color = color;
+    this.isAttacking;
   }
 
   draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 50, this.height);
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    //attack box
+    if (this.isAttacking) {
+      c.fillStyle = "green";
+      c.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height,
+      );
+    }
   }
 
   update() {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.toEarth();
@@ -33,6 +58,13 @@ class Sprite {
   toEarth() {
     this.position.y = canvas.height - this.height;
     this.velocity.y = 0;
+  }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100);
   }
 }
 
@@ -45,6 +77,10 @@ const player = new Sprite({
     x: 0,
     y: 0,
   },
+  offset: {
+    x: 0,
+    y: 0,
+  },
 });
 
 const enemy = new Sprite({
@@ -54,6 +90,11 @@ const enemy = new Sprite({
   },
   velocity: {
     x: 0,
+    y: 0,
+  },
+  color: "blue",
+  offset: {
+    x: -50,
     y: 0,
   },
 });
@@ -72,6 +113,15 @@ const keys = {
     pressed: false,
   },
 };
+
+function rectangluarCollision({ rec1, rec2 }) {
+  return (
+    rec1.attackBox.position.x + rec1.attackBox.width >= rec2.position.x &&
+    rec1.attackBox.position.x <= rec2.position.x + rec2.width &&
+    rec1.attackBox.position.y + rec1.attackBox.height >= rec2.position.y &&
+    rec1.attackBox.position.y <= rec2.position.y + rec2.height
+  );
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -110,15 +160,28 @@ function animate() {
   } else {
     enemy.velocity.x = 0;
   }
+
+  if (
+    rectangluarCollision({ rec1: player, rec2: enemy }) &&
+    player.isAttacking
+  ) {
+    player.isAttacking = false;
+    console.log("go");
+  }
+
+  if (
+    rectangluarCollision({ rec1: enemy, rec2: player }) &&
+    enemy.isAttacking
+  ) {
+    enemy.isAttacking = false;
+    console.log("enemy attack successful");
+  }
 }
 
 animate();
 
 window.addEventListener("keydown", (event) => {
-  console.log("keydown" + event.key);
-    
   switch (event.key) {
-    //player event
     case "a":
       keys.a.pressed = true;
       player.lastKey = "a";
@@ -143,6 +206,9 @@ window.addEventListener("keydown", (event) => {
     case "ArrowUp":
       enemy.velocity.y = -20;
       break;
+    case "ArrowDown":
+      enemy.isAttacking = true;
+      break;
   }
 });
 
@@ -157,6 +223,9 @@ window.addEventListener("keyup", (event) => {
       break;
     case "w":
       break;
+    case " ":
+      player.attack();
+      break;
 
     //enemy event
     case "ArrowRight":
@@ -166,5 +235,4 @@ window.addEventListener("keyup", (event) => {
       keys.ArrowLeft.pressed = false;
       break;
   }
-  console.log("keyup" + event.key);
 });
