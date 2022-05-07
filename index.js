2c4cf247dc4cdc37e3f38b9fdc44e73c6504e639
JinYeongKim <handayoung11@gmail.com>
2022-05-08 00:28:34 +0900

    배경화면 설정

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/background.png",
+});
 const player = new Fighter({
   position: {
     x: 0,
