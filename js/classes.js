c528a1851f5af8b6cd8c01dae3dcb39c80d32280
JinYeongKim <handayoung11@gmail.com>
2022-05-10 18:46:09 +0900

    Player Sprite 기본(서있는 상태) 이미지 지정

--- js/classes.js
+++ js/classes.js
@@ -4,16 +4,17 @@ class Sprite {
     imageSrc,
     scale = 1,
     framesMax = 1,
-    framesCurrent = 0,
+    offset = { x: 0, y: 0 },
   }) {
     this.position = position;
     this.image = new Image();
     this.image.src = imageSrc;
     this.scale = scale;
     this.framesMax = framesMax;
-    this.framesCurrent = framesCurrent;
+    this.framesCurrent = 0;
     this.framesElapsed = 0;
     this.framesHold = 5;
+    this.offset = offset;
   }
 
   draw() {
@@ -23,16 +24,15 @@ class Sprite {
       0,
       this.renderingWidth,
       this.image.height,
-      this.position.x,
-      this.position.y,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
       this.renderingWidth * this.scale,
       this.image.height * this.scale,
     );
     this.renderingWidth = this.image.width / this.framesMax;
   }
 
-  update() {
-    this.draw();
+  animateFrames() {
     this.framesElapsed++;
     if (this.framesElapsed % this.framesHold == 0) {
       if (this.framesCurrent < this.framesMax - 1) {
@@ -42,11 +42,31 @@ class Sprite {
       }
     }
   }
+
+  update() {
+    this.draw();
+    this.animateFrames();
+  }
 }
 
-class Fighter {
-  constructor({ position, velocity, color = "red", offset }) {
-    this.position = position;
+class Fighter extends Sprite {
+  constructor({
+    position,
+    velocity,
+    color = "red",
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset,
+    sprites,
+  }) {
+    super({
+      position,
+      imageSrc,
+      scale,
+      framesMax,
+      offset,
+    });
     this.velocity = velocity;
     this.height = 150;
     this.width = 50;
@@ -64,24 +84,25 @@ class Fighter {
     this.health = 100;
   }
 
-  draw() {
-    c.fillStyle = this.color;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  // draw() {
+  //   c.fillStyle = this.color;
+  //   c.fillRect(this.position.x, this.position.y, this.width, this.height);
 
-    //attack box
-    if (this.isAttacking) {
-      c.fillStyle = "green";
-      c.fillRect(
-        this.attackBox.position.x,
-        this.attackBox.position.y,
-        this.attackBox.width,
-        this.attackBox.height,
-      );
-    }
-  }
+  //   //attack box
+  //   if (this.isAttacking) {
+  //     c.fillStyle = "green";
+  //     c.fillRect(
+  //       this.attackBox.position.x,
+  //       this.attackBox.position.y,
+  //       this.attackBox.width,
+  //       this.attackBox.height,
+  //     );
+  //   }
+  // }
 
   update() {
     this.draw();
+    this.animateFrames();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
